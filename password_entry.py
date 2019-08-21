9a8092d8f9703aeac26f704a5141ddebad987c8d
jc545730 <jordan.rose@my.jcu.edu.au>
2019-08-21 14:46:55 +1000

    Add fail pass check

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,12 @@
 """  Jordan Rose  """
+
+import getpass
+
+password = input("Please enter a password\n"
+                 "Must be at least 6 characters")
+while len(password) < 6:
+    password = input("Password must be at least 6 characters")
+
+getpass.getpass()
+
+print(password)
